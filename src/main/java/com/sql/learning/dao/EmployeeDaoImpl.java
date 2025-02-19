6a9231f38e3ffa0364edf2764a0f76792b225da1
pancham01 <pancham9557@gmail.com>
2025-02-19 19:09:43 +0530

    update

--- src/main/java/com/sql/learning/dao/EmployeeDaoImpl.java
+++ src/main/java/com/sql/learning/dao/EmployeeDaoImpl.java
@@ -10,6 +10,7 @@ public class EmployeeDaoImpl implements EmployeeDao {
 	static Statement stmt = null;
 	static final String INSERT_QUERY = "INSERT INTO EMPLOYEE(ID, NAME , GENDER , SALARY) VALUES(%d,'%s','%s',%d)";
 	static final String UPDATE_QUERY = "UPDATE EMPLOYEE SET NAME = '%s', GENDER = '%s' , SALARY = %d WHERE ID = %d";
+	static final String DELETE_QUERY = "DELETE FROM EMPLOYEE WHERE ID = %d";
 
 	static {
 		try {
@@ -21,21 +22,21 @@ public class EmployeeDaoImpl implements EmployeeDao {
 
 	@Override
 	public void saveEmployee(Employee e) throws SQLException {
-		stmt.executeUpdate(String.format(INSERT_QUERY, e.getId(),e.getName(),e.getGender(),e.getSalary()));
-		System.out.println(String.format(INSERT_QUERY, e.getId(),e.getName(),e.getGender(),e.getSalary()));
+		stmt.executeUpdate(String.format(INSERT_QUERY, e.getId(), e.getName(), e.getGender(), e.getSalary()));
+		System.out.println(String.format(INSERT_QUERY, e.getId(), e.getName(), e.getGender(), e.getSalary()));
 	}
 
 	@Override
 	public void updateEmployee(Employee e) throws SQLException {
-		stmt.executeUpdate(String.format(UPDATE_QUERY, e.getName(),e.getGender(),e.getSalary(),e.getId()));
-		
-		System.out.println(String.format(UPDATE_QUERY, e.getName(),e.getGender(),e.getSalary(),e.getId()));
+		stmt.executeUpdate(String.format(UPDATE_QUERY, e.getName(), e.getGender(), e.getSalary(), e.getId()));
+
+		System.out.println(String.format(UPDATE_QUERY, e.getName(), e.getGender(), e.getSalary(), e.getId()));
 	}
 
 	@Override
-	public void deleteEmployeeById(int id) {
-		// TODO Auto-generated method stub
-
+	public void deleteEmployeeById(int id) throws SQLException {
+		stmt.executeUpdate(String.format(DELETE_QUERY, id));
+		System.out.println((String.format(DELETE_QUERY, id)));
 	}
 
 	@Override
